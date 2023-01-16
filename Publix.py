3b1ed845546f3b3fcc3adac4f19ca4aa43d43ff7
RMB2023 <122807557+RMB2023@users.noreply.github.com>
2023-01-16 16:21:15 -0500

    Update Publix.py

--- Publix.py
+++ Publix.py
@@ -1,15 +1,30 @@
 from selenium import webdriver
 import time
 import bs4 as bs
+import pandas as pd
+import yagmail
 
-# Variables:
+##### Settings #####
+store_list = [
+    ["Mcalister Square","https://www.publix.com/locations/602-mcalister-square"],
+    ["Center Point","https://www.publix.com/locations/205-center-point"],
+    ["McBee Station","https://www.publix.com/locations/1148-mcbee-station"]
+    ]
+GMAIL_username = 'XXXXXXXXX@gmail.com'
+GMAIL_password = 'XXXXXXXXX'
+target_email = 'XXXXXXXXX@gmail.com'
+deal_dict = {} # Create empty dictionary to store deals by store
+
+##### XPath Variables #####
 loadmore = '//*[@id="main"]/div[4]/div/div[2]/div[2]/div[4]/button/span'
-web_driver_path = r'C:\Users\YOUR PATH HERE'
+web_driver_path = r'C:\Users\ryanb\OneDrive\Desktop\chromedriver_win32\chromedriver.exe'
+shop_at_this_store = '//*[@id="main"]/div[3]/div[1]/button'
 
+# Create selenium webdriver
 browser = webdriver.Chrome(executable_path = web_driver_path)
 
-browser.get("https://www.publix.com/savings/weekly-ad/bogo")
 
+# Function to click on xpath elements
 def clickButton(path):
     try:
         browser.find_element_by_xpath(path).click()
@@ -18,32 +33,51 @@ def clickButton(path):
         time.sleep(1.0)
         clickButton(path)
 
-time.sleep(3.0)
 
-# Try clicking "load more" button twice
-clickButton(loadmore)
-clickButton(loadmore)
+# Get the deals from each store and assign to deal_dict
+for store in store_list:
 
-time.sleep(1.0)
+    # Go to store page 
+    browser.get(store[1])
+    time.sleep(1.0)
 
-# Get page source
-html = browser.page_source
+    # Select store
+    clickButton(shop_at_this_store)
+    time.sleep(3.0)
+       
+    # Go to BOGO page
+    browser.get("https://www.publix.com/savings/weekly-ad/bogo")
+
+    # Try clicking "Load More" button twice
+    clickButton(loadmore)
+    clickButton(loadmore)
+
+    # Get page source
+    html = browser.page_source
+    soup = bs.BeautifulSoup(html)
+
+    # Find all BOGO deals
+    product_names = soup.findAll('span', {'class': 'p-text paragraph-md normal context--default color--null line-clamp title'})
+
+    # Create empty list for each BOGO deal
+    deal_list = []
+    
+    # Add BOGO products to list
+    for i in range(len(product_names)):
+        deal_list.append(product_names[i].text.strip())
+
+    # Add store name and BOGO products to dict
+    deal_dict[store[0]] = deal_list
+
 
 # Close browser
 browser.close()
 
-# soup = bs.BeautifulSoup(html, features='html5lib')
-soup = bs.BeautifulSoup(html)
+# Convert dictionary to pandas dataframe to later on use pandas built in to_html function
+deals_DF = pd.DataFrame.from_dict(data = deal_dict, orient = 'columns')
 
-# Find all BOGO deals
-product_names = soup.findAll('span', {'class': 'p-text paragraph-md normal context--default color--null line-clamp title'})
-
-# Create empty dictionary to store deals
-deal_dict = {}
-
-# Add BOGO products to dictionary
-for i in range(len(product_names)):
-    deal_dict[i] = product_names[i].text.strip()
-
-# Print deals
-print(deal_dict)
+# Email BOGO deals
+yag = yagmail.SMTP(GMAIL_username , GMAIL_password)
+subject = 'Publix BOGO Deals'
+body = deals_DF.to_html(justify = 'center' , col_space = 20) # Convert dataframe to HTML
+yag.send(to = target_email, subject = subject, contents = body)
